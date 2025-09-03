10acef32f57b1ba2f259c3e5cf4e033a9d09594f
Dylan Soll <22dylansoll@gmail.com>
2025-09-03 12:41:00 +1000

    Implemented test coverage for bitvector size and capacity methods

--- test/TestBitVector.java
+++ test/TestBitVector.java
@@ -2,6 +2,8 @@
  * Supplied by the COMP3506/7505 teaching team, Semester 2, 2025.
  */
 
+import org.junit.Assert;
+import org.junit.Test;
 import uq.comp3506.a1.structures.BitVector;
 
 public class TestBitVector {
@@ -11,4 +13,24 @@ public class TestBitVector {
         System.out.println("Success!");
     }
 
+    @Test
+    public void testSize() {
+        BitVector vec = new BitVector(10L);
+        Assert.assertEquals(10L, vec.size());
+        vec = new BitVector(64L);
+        Assert.assertEquals(64L, vec.size());
+        vec = new BitVector(15331L);
+        Assert.assertEquals(15331L, vec.size());
+    }
+
+
+    @Test
+    public void testCapacity() {
+        BitVector vec = new BitVector(10L);
+        Assert.assertEquals(BitVector.BitsPerElement *  ((long) Math.ceil(((double) (10L) / BitVector.BitsPerElement))), vec.capacity());
+        vec = new BitVector(64L);
+        Assert.assertEquals(BitVector.BitsPerElement * ((long) Math.ceil(((double) (64L) / BitVector.BitsPerElement))), vec.capacity());
+        vec = new BitVector(15331L);
+        Assert.assertEquals(BitVector.BitsPerElement * ((long) Math.ceil(((double) (15331L) / BitVector.BitsPerElement))), vec.capacity());
+    }
 }
